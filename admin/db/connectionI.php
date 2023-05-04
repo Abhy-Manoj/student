573ce680b493b746abcc736e22d1689ab8084a8d
Abhy-Manoj <69760673+Abhy-Manoj@users.noreply.github.com>
2023-05-04 13:45:20 +0530

    Update connectionI.php

--- admin/db/connectionI.php
+++ admin/db/connectionI.php
@@ -1,6 +1,10 @@
 <?php
 //session_start();
 date_default_timezone_set("Asia/Calcutta");
-
-$con=mysqli_connect("localhost","root","","student_learning");
+$servername = "containers-us-west-177.railway.app";
+;$username = "root";
+$password = "vLAW5CRDbAmIuhGGFvX8";
+$dbname = "railway";
+$port = 7687;
+$con = mysqli_connect($servername, $username, $password, $dbname,$port);
 ?>
